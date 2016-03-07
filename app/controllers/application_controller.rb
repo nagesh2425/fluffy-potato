f67f1242c410c917c522ad3d2b5024c5c2bbe666
Nagesh <nsangana@linkedin.com>
2016-03-07 13:28:42 +0530

    Task #8 In the footer, under the LATEST section, displayed the 5 most recent songs released and used caching on the page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,14 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
-
+  before_action :get_latest_five_songs
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #method to get latest 5 songs to display in footer
+  def get_latest_five_songs
+      @latest_songs = Song.recent(5)
+  end
+  
 end
